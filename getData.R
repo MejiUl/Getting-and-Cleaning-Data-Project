16ac87c21f2eef4ed81448ce5a8584f7502e861a
MejiUlises <jonhylargo@gmail.com>
2014-06-21 12:56:14 -0500

    First Iteration of the script

---
+++ getData.R
@@ -0,0 +1,9 @@
+#
+#
+#
+#We read all the table we will be using. 
+subjectTrain <- read.table("subject_train.txt")             
+XTrain <- read.table("X_Train.txt")
+YTrain <- read.table("Y_Train.txt")
+
+
